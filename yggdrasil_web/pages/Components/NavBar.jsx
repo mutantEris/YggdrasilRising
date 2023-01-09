d73b3b521eb99095b6644ce34ce9c98a7d214351
ronroeandassociates <ron@roeandassociates.com>
2023-01-08 19:51:53 -0600

    fix: remove return

--- yggdrasil_web/pages/Components/NavBar.jsx
+++ yggdrasil_web/pages/Components/NavBar.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image"
 import react from "react"
+import Link from "next/link"
 
 const NavBar = () => {
-  return
+  //return
   ;<div className="fixed w-full h-20 shadow xl z-[100]">
     <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
       <Image
@@ -27,7 +28,8 @@ const NavBar = () => {
       </Link>
       <Link href="/">
         <li className="ml-10 text sm uppercase hover:border-b">Contact</li>
-      </Link>      <Link href="/">
+      </Link>{" "}
+      <Link href="/">
         <li className="ml-10 text sm uppercase hover:border-b">Contact</li>
       </Link>
     </ul>
