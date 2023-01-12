ad99a97df1c643d4d250f7b7eb6aac46e8e7051e
ronroeandassociates <ron@roeandassociates.com>
2023-01-12 10:29:02 -0600

    arrrrrhhhhh~

--- yggdrasil_web/pages/Components/NavBar.jsx
+++ yggdrasil_web/pages/Components/NavBar.jsx
@@ -1,36 +1,53 @@
-import Image from "next/image"
-import react from "react"
-import Link from "next/link"
-// import AiOutlineMenu from "react-icons-ai"
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons-ai";
 
 const NavBar = () => {
   return (
-    <div className="fixed w-full h-20 shadow xl z-[100]">
-      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Image src="/" alt="/" width="125" height="125" />
+      <div className="fixed w-full h-20 bg-slate-300 shadow-xl z-[100]">
+          <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+            <Image
+              src="/../public/NavLogo.jpg"
+              alt="/"
+              width="125"
+              height="125"
+            />
+          </div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/about">
+              <li className="ml-10 text sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/gallery">
+              <li className="ml-10 text sm uppercase hover:border-b">
+                Gallery
+              </li>
+            </Link>
+            <Link href="/chat">
+            <li className="ml-10 text sm uppercase hover:border-b">
+                Gallery
+              </li>
+            </Link>
+            <Link href="/contact">
+              <li className="ml-10 text sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+            <Link href="/signin">
+              <li className="ml-10 text sm uppercase hover:border-b">
+                Sign In
+              </li>
+            </Link>
+          </ul>
+          </div>
+
+</>
+<div className="md:hidden">
+        <AiOutlineMenu />
       </div>
-      <ul className="hidden md:flex">
-        <Link href="/">
-          <li className="ml-10 text sm uppercase hover:border-b">Home</li>
-        </Link>
-        <Link href="/about">
-          <li className="ml-10 text sm uppercase hover:border-b">About</li>
-        </Link>
-        <Link href="/gallery">
-          <li className="ml-10 text sm uppercase hover:border-b">Gallery</li>
-        </Link>
-        <Link href="/chat">
-          <li className="ml-10 text sm uppercase hover:border-b">Chat</li>
-        </Link>
-        <Link href="/contact">
-          <li className="ml-10 text sm uppercase hover:border-b">Contact</li>
-        </Link>
-        <Link href="/signin">
-          <li className="ml-10 text sm uppercase hover:border-b">Sign In</li>
-        </Link>
-      </ul>
-    </div>
-  )
 }
 
-export default NavBar
+export default NavBar;
