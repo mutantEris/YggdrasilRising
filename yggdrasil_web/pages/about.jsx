f869bb242560783050deaf6f048c8f10344cdb3e
ronroeandassociates <ron@roeandassociates.com>
2023-01-14 21:33:14 -0600

    end of day push

--- yggdrasil_web/pages/about.jsx
+++ yggdrasil_web/pages/about.jsx
@@ -7,7 +7,7 @@ const About = () => {
 		<div className='w-full md:h-screen p-2 flex items-center py-16'>
 			<div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
 				<div className='col-span-2'>
-					<p>About</p>
+					<p>What</p>
 					<h2>How do you play the game?</h2>
 					<p>
 						Yggdrasil Rising is a turn-based strategy game. Each turn, you can
